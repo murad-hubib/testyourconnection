000e934307738b1c939d14c48a43a9db09acb63c
murad-hubib <murad.habib@arbisoft.com>
2022-07-18 15:52:56 +0500

    MEI-21279 CSS revamped and UI

---
+++ static/js/custom.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    
+});
+
+$(window).load(function(){
+   function show_popup(){
+      $("#overlayer").delay(2000).css("display", "none");
+   };
+   window.setTimeout( show_popup, 5000 ); // 5 seconds
+})
